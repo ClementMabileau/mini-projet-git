e022a2d6de07a92952cd99d9449c2d07fc08b024
Fares AMROUD <fares.amroud@etudiant.univ-brest.fr>
2026-02-18 11:03:43 +0100

    feat: ajout des raisons et affichage aléatoire

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+raisons = [
+    "parce que la vie est dure",
+    "parce que j’ai la flemme",
+    "parce que c’est gratuit"
+]
+
+print(random.choice(sujets),
+      random.choice(objets),
+      random.choice(raisons))
