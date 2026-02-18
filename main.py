59027f8bbffdaa33e383b2b29ff2c85c7c9edf69
ktambon <kevin.tambon@hotmail.fr>
2026-02-18 11:42:45 +0100

    fix : Correction affichage accents + Ajout auteur Kevin dans ReadMe

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import random
 
 objets = [
-    "des pâtes",
+    "des pates",
     "une pizza froide",
     "un kebab douteux"
 ]
 
 sujets = [
-    "Aujourd’hui je mange",
+    "Aujourd'hui je mange",
     "Ce soir je commande",
     "Demain je cuisine"
 ]
 
 raisons = [
     "parce que la vie est dure",
-    "parce que j’ai la flemme",
-    "parce que c’est gratuit"
+    "parce que j'ai la flemme",
+    "parce que c'est gratuit"
 ]
 
 print(random.choice(sujets),
